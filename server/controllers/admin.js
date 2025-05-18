5b3034d02de8383f8d4f597219f640fe3337939d
kayse Dahir Ali <kaysadahir@gmail.com>
2025-05-18 20:00:23 +0600

    fix: modernize Navbar icons

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -15,7 +15,7 @@ export const adminLogin = async (req, res, next) => {
       res.cookie("adminToken", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
       });
       return res
         .status(200)
